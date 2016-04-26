9f3eed5061c75346ead23e4fd21911756f852ac0
rensramos <rensramos17@gmail.com>
2016-04-26 15:49:51 +0800

    Update slider

--- js/home.js
+++ js/home.js
@@ -3,6 +3,7 @@ bsi.init = function() {
     bsi.initImgSlider();
 };
 
+
 bsi.initImgSlider = function() {
     var owl = $('.slider').owlCarousel({
         lazyLoad: true,
@@ -28,12 +29,6 @@ bsi.initImgSlider = function() {
             }
         }
     });
-
-    // Other Slides
-    var pageImage = $('#page-container .right-section .slider .owl-stage-outer .owl-stage .owl-item .item .image-container .page-image');
-
-    //pageImage.addClass('animated fadeIn');
-
     owl.on('changed.owl.carousel',function(property){
         var current = property.item.index;
         var rank = $(property.target).find(".owl-item").eq(current).find('li').data('rank');
@@ -45,8 +40,8 @@ bsi.initImgSlider = function() {
         $(".section-description").fadeOut(function() {
             $(this).html(description).fadeIn();
         });
-
     });
+
 };
 
 
